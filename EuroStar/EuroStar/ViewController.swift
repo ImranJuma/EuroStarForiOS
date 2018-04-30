fe0df8f15f49cdb2e5edf2aff7476e7577090ca6
Imran Juma <imranjuma49@gmail.com>
2018-04-30 01:36:38 -0400

    First Upload Of Project Files
    First Upload of Project Files, Home UIController Built and setting up buttons in progress

---
+++ EuroStar/EuroStar/ViewController.swift
@@ -0,0 +1,58 @@
+//
+//  ViewController.swift
+//  EuroStar
+//
+//  Created by Imran Juma on 2018-04-29.
+//  Copyright © 2018 Imran Juma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    //Weak varibles made so I can round the corners of each buttons being used
+    @IBOutlet weak var servicesButton: UIButton!
+    @IBOutlet weak var portfolioButton: UIButton!
+    @IBOutlet weak var aboutUsButton: UIButton!
+    @IBOutlet weak var contactUsButton: UIButton!
+    @IBOutlet weak var socialLinksButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // Actions for our Services Button
+    @IBAction func servicesButton(_ sender: Any) {
+        //
+        // TODO
+        //
+    }
+    
+    // Actions for our Portfolio Button
+    @IBAction func portfolioButton(_ sender: Any) {
+        //
+        // TODO
+        //
+    }
+    
+    // Actions for our About-Us Button
+    @IBAction func aboutUsButton(_ sender: Any) {
+        //
+        // TODO
+        //
+    }
+    
+    // Actions for our Contact-Us Button\
+    @IBAction func contactUsButton(_ sender: Any) {
+        //
+        // TODO
+        //
+    }
+}
+
