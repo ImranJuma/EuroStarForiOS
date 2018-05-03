f35de50787256b281a6f3aee59ac786403b97d9a
Imran Juma <imranjuma49@gmail.com>
2018-05-03 00:04:30 -0400

    Portfolio Update
    Today we will be working on updating the portfolio for our client.
    
    Here are the following updates that we have made:
    - Designed collection view based on the different cells that we have
    - Added PortfolioCollectionViewController File
    - Added PortfolioCollectionViewControllerCell File

---
+++ EuroStar/EuroStar/Portfolio/PortfolioCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  PortfolioCollectionViewCell.swift
+//  EuroStar
+//
+//  Created by Imran Juma on 2018-05-03.
+//  Copyright © 2018 Imran Juma. All rights reserved.
+//
+
+import UIKit
+
+class PortfolioCollectionViewCell: UICollectionViewCell {
+    
+}
