40fefeae7e84daeee4e4fca26dd0413a9a944031
Imran Juma <imranjuma49@gmail.com>
2018-05-03 01:07:04 -0400

    Resuming Previous Update
    Resuming the current update to add additional feautures
    - Changed background image within the portfolio to reflect a better view for the user
    - I have also explanded the string to allow for scrolling so if the client wants to add various dimages to the to the application he can make the portfolio as long as he wants to
    - We are going to modify this object so that it works on bigger and smaller devices
    - We have now added support for the portfolio class for the following devices
    - iPhone SE / 5S / 5C Modles
    - iPhone 6 / 6S / 7 / 8 Modles
    - iPhone 6+ / 6S+ / 7+ / 8+ Modles
    - iPad Mini (All Generations)
    - iPad (9.7 All generations)
    - iPad (10.2 All generations)
     - iPad (12.5 All generations)
    - Support for iPhone X will come in the next update when we work on our constrainsts
    - That is all for this update, tomorrow we will work on the contact us page and possibly the socail links page

--- EuroStar/EuroStar/Portfolio/PortfolioCollectionViewController.swift
+++ EuroStar/EuroStar/Portfolio/PortfolioCollectionViewController.swift
@@ -10,16 +10,19 @@ import UIKit
 
 private let reuseIdentifier = "Cell"
 
-class PortfolioCollectionViewController: UICollectionViewController {
+//Adding FlowDelate will allow us to resize based on the users device
+class PortfolioCollectionViewController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
     
     //Here using strings we will use this to populate all the different images to load within the collection view
-    var imageList = ["Collection_View_Image_1", "Collection_View_Image_2", "Collection_View_Image_3", "Collection_View_Image_4", "Collection_View_Image_5"]
+    var imageList = ["Collection_View_Image_1", "Collection_View_Image_2", "Collection_View_Image_3", "Collection_View_Image_4", "Collection_View_Image_5", "Collection_View_Image_1", "Collection_View_Image_2", "Collection_View_Image_3", "Collection_View_Image_4", "Collection_View_Image_5", "Collection_View_Image_1", "Collection_View_Image_2", "Collection_View_Image_3", "Collection_View_Image_4", "Collection_View_Image_5"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
+        
+        collectionView?.backgroundView = UIImageView(image: UIImage(named: "Background Image"))
 
         // Do any additional setup after loading the view.
     }
@@ -62,6 +65,72 @@ class PortfolioCollectionViewController: UICollectionViewController {
         cell.cellImage.image = UIImage(named: imageList[indexPath.row])
     
         return cell
+        
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        // The bounds of the collection view will now be based upon the the size of the users device
+        let screenSize : CGRect = UIScreen.main.bounds
+        
+        var widthCell = 0
+        var heightCell = 0
+        
+        //Refrence the screen size to a specific measurement, we will base this off of the iPhone SE - iPhone 8
+        
+        
+        // This will be for iPhone SE
+        if screenSize.width == 320 {
+            
+            widthCell = 130
+            heightCell = 130
+            
+        }
+        
+        //This will be for iPhone 6-7-8 NOT PLUS Sizes, These will be added later on
+        if screenSize.width == 375 {
+            
+            widthCell = 160
+            heightCell = 160
+            
+        }
+        
+        //This will be for iPhone 6-6S-7-8 Plus Sizes, These will be added later on
+        if screenSize.width == 414 {
+            
+            widthCell = 180
+            heightCell = 180
+            
+        }
+        
+        //This will be for iPhone 6-6S-7-8 Plus Sizes, These will be added later on
+        if screenSize.width == 768 {
+            
+            widthCell = 230
+            heightCell = 230
+            
+        }
+        
+        //This will be for iPhone 6-6S-7-8 Plus Sizes, These will be added later on
+        if screenSize.width == 834 {
+            
+            widthCell = 250
+            heightCell = 250
+            
+        }
+        
+        //This will be for iPhone 6-6S-7-8 Plus Sizes, These will be added later on
+        if screenSize.width == 1024 {
+            
+            widthCell = 310
+            heightCell = 310
+            
+        }
+        // This same method can be done for iPad as-well
+        
+        
+        
+        return CGSize(width: widthCell, height: heightCell) // These varibles come from the different devices we tested on, this means that no matter what device we use, the user will be able to read the size needed
     }
 
     // MARK: UICollectionViewDelegate
