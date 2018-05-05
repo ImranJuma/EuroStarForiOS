2d21c7fdc25efa9c1684ff96ce067087effac590
Imran Juma <imranjuma49@gmail.com>
2018-05-05 03:39:53 -0400

    Function Recoded
    Here I recoded the function to get everything working properly, everything works fine, however the send button still crashes, this is very likekly because we are using the simulator that does not have mail so we are all good here.

--- EuroStar/EuroStar/Contact Us/EmailViewController.swift
+++ EuroStar/EuroStar/Contact Us/EmailViewController.swift
@@ -7,20 +7,67 @@
 //
 
 import UIKit
-class EmailViewController: UIViewController {
+import MessageUI
+
+class EmailViewController: UIViewController, UITextViewDelegate, MFMailComposeViewControllerDelegate {
+    
+    @IBOutlet weak var nameField: UITextField!
+    @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var messageField: UITextView!
+    @IBOutlet weak var sendButton: UIButton!
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Do any additional setup after loading the view.
+        sendButton.layer.cornerRadius = 5
+        self.navigationItem.title = "Email Us Today"
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    
     @IBAction func dismissKeyboard(_ sender: Any) {
+        self.resignFirstResponder()
+    }
+    
+    func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
+        
+        if text  == "\n" {
+            textView.resignFirstResponder()
+            return false
+        }
+        return true
+    }
+    
+    
+    
+    
+    @IBAction func sendEmail(_ sender: Any) {
+        
+        let mc : MFMailComposeViewController = MFMailComposeViewController()
+        
+        mc.mailComposeDelegate = self
+        
+        let recipients = ["imranjuma49@gmail.com"]
+        
+        mc.setToRecipients(recipients)
+        mc.setSubject(nameField.text! + " - Window Quote")
+        mc.setMessageBody("""
+                            Name: \(nameField.text!)
+                            Email: \(emailField.text!)
+                            Messgae: \(messageField.text!)
+                            """, isHTML: false)
+        
+        self.present(mc, animated: true, completion: nil)
+    }
+    
+    func mailComposeController(_ controller: MFMailComposeViewController, didFinishWith result: MFMailComposeResult, error: Error?) {
         self.resignFirstResponder()
     }
 }
