5a5f4164f25364581404f132deaad7082686aa69
Imran Juma <imranjuma49@gmail.com>
2018-05-05 03:02:04 -0400

    Fixing Message Box
    - Here I fixed the messagebox as it was not appearing when the client wanted to send a message, rebuilt the page and not it working
    - When we hit the send button the application crashes, this is most likly due to the fact that the test device does not support the email so we will check this again tomorrow when we run on a real device

--- EuroStar/EuroStar/Contact Us/EmailViewController.swift
+++ EuroStar/EuroStar/Contact Us/EmailViewController.swift
@@ -12,11 +12,13 @@ import MessageUI
 class EmailViewController: UIViewController, UITextViewDelegate, MFMailComposeViewControllerDelegate {
     
     //OutletSection
+   // @IBOutlet weak var nameField: UITextField!
     @IBOutlet weak var nameField: UITextField!
     @IBOutlet weak var emailField: UITextField!
-    @IBOutlet weak var messageField: UITextView!
     @IBOutlet weak var messageButton: UIButton!
-
+    @IBOutlet weak var messageField: UITextView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -30,11 +32,11 @@ class EmailViewController: UIViewController, UITextViewDelegate, MFMailComposeVi
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-    @IBAction func dismissKeybaord(_ sender: Any) {
-        
+
+    @IBAction func dismissKeyboard(_ sender: Any) {
         self.resignFirstResponder()
     }
+    
     
     func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
         
@@ -55,17 +57,19 @@ class EmailViewController: UIViewController, UITextViewDelegate, MFMailComposeVi
         // Who we are sending the email to, this can be further changed letter
         let recipients = ["imranjuma49@gmail.com"]
         mc.setToRecipients(recipients)
-       
+        
         // How the email will be presented when the user sends the messgae
         mc.setSubject(nameField.text! + " - Window Quote")
         mc.setMessageBody("""
-                                Hello My Name Is: \(nameField.text!)
-                                I can Be Reached At: \(emailField.text!)
-                                Here Is What I Am Looking For: \(messageField.text!)
-                            """, isHTML: false)
+            Hello My Name Is: \(nameField.text!)
+            I can Be Reached At: \(emailField.text!)
+            Here Is What I Am Looking For: \(messageField.text!)
+            """, isHTML: false)
         
         self.present(mc, animated:true, completion: nil)
+        
     }
+    
     
     //Allowing the text to go away, when we are done sending the message
     func mailComposeController(_ controller: MFMailComposeViewController, didFinishWith result: MFMailComposeResult, error: Error?) {
